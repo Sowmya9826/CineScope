5acb47b445dc41dad7dfcd25c42996b14b2034dd
Sowmya <sowmya@Sowmyas-MacBook-Pro.local>
2025-03-23 23:00:05 -0700

    setup  mongodb cluster Created Movie Services,Movie Repository,Movie Controller Implemented MovieService methods to fetch movie by internal ID and imdbId

---
+++ src/main/java/dev/sowmya/CineScope/CineScopeApplication.java
@@ -0,0 +1,16 @@
+package dev.sowmya.CineScope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+
+public class CineScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sowmya/CineScope/CineScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sowmya.CineScope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
